c520dc29048ef67b2b19a5cded5ec1442011fa27
Parviz <perviz822@gmail.com>
2025-10-04 22:45:05 +0200

    pattern subscribe (pong.*)

--- understand/redis_pubsup.py
+++ understand/redis_pubsup.py
@@ -2,23 +2,23 @@ import asyncio, redis.asyncio as redis, json
 
 async def  publish_to_channel(r):
      await asyncio.sleep(7)
-     await r.publish("ping",json.dumps({"msg":"pong"}))
+     await r.publish("ping.demo",json.dumps({"msg":"pong"}))
 
 
 async def  publish_to_channel_2(r):
      await asyncio.sleep(10)
-     await r.publish("ping",json.dumps({"msg":"pong"}))     
+     await r.publish("ping.demo",json.dumps({"msg":"pong"}))     
      
 
 
 async def main():
     r = redis.from_url("redis://localhost:6379")
     pubsub = r.pubsub()
-    await pubsub.subscribe("ping") # subscribing to  channel ping
+    await pubsub.psubscribe("ping.*") # subscribing to  channel ping
     asyncio.create_task(publish_to_channel(r))
     asyncio.create_task(publish_to_channel_2(r))
     async for m in pubsub.listen():
-         if m["type"] =="message":
+         if m["type"] =="pmessage":
               print("Got the message")
               
     await pubsub.close()          
